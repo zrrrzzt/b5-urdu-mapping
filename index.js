c3ceba92c507fbd3958f720838fc287ceef15351
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-02-01 20:32:58 +0100

    Adds scoring of result

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+const calculateScore = require('@alheimsins/bigfive-calculate-score')
 const repackQuestions = require('./lib/repack-questions')
 
 module.exports = async (request, response) => {
-  const data = await request.body
-  const questions = JSON.parse(JSON.stringify(data))
+  const post = await request.body
+  const questions = JSON.parse(JSON.stringify(post))
   const answers = repackQuestions(questions)
-  console.log(answers)
-  response.json(answers)
+  const score = calculateScore({ answers })
+  const converted = {
+    lang: 'ur',
+    test: 'costa-mccrae-300-ipip-neo-pi-r',
+    totalQuestions: 300,
+    nickname: questions.Nick,
+    age: questions.Age,
+    sex: questions.Sex,
+    country: questions.Country,
+    honestly: questions.A,
+    allAnswered: questions.B,
+    correct: questions.C,
+    answers,
+    score
+  }
+  response.json(converted)
 }
