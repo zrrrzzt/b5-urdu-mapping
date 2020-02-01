6a1926dddc70d852d3bc5d500c5f1ba57549e5f8
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-02-01 20:02:24 +0100

    Implements repack an mapping of questions

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const repackQuestions = require('./lib/repack-questions')
+
 module.exports = async (request, response) => {
   const data = await request.body
-  const result = JSON.parse(JSON.stringify(data))
-  console.log(result)
-  response.json(result)
+  const questions = JSON.parse(JSON.stringify(data))
+  const answers = repackQuestions(questions)
+  console.log(answers)
+  response.json(answers)
 }
